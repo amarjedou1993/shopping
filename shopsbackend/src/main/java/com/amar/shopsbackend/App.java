38dbcab4ec4fbc05133caf6851cf60cb688d990f
Amar Amar <amarjedoumaman@gmail.com>
2018-09-26 11:18:56 +0000

    Create First Step

---
+++ shopsbackend/src/main/java/com/amar/shopsbackend/App.java
@@ -0,0 +1,13 @@
+package com.amar.shopsbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
